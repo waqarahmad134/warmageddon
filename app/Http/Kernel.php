a90c660048ce90092935962429960534d9e0d8ec
waqar ahmad <waqar4704649@gmail.com>
2025-08-20 05:41:17 +0500

    handling cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,6 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             'throttle:60,1',
             'bindings',
-            \App\Http\Middleware\CorsMiddleware::class, 
         ],
     ];
 
