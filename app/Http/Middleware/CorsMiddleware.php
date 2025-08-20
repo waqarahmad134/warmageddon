3190fe79e09b61f9fb11f2b4f3c09e0dbe874ee3
waqar ahmad <waqar4704649@gmail.com>
2025-08-20 06:02:17 +0500

    handling cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,19 +9,16 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $headers = [
-            'Access-Control-Allow-Origin'      => '*', // or your specific domain
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'     => 'Content-Type, X-Auth-Token, Origin, Authorization',
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
         ];
 
-        // If OPTIONS request, return immediately with headers
         if ($request->isMethod('OPTIONS')) {
             return response()->json('OK', 200, $headers);
         }
 
         $response = $next($request);
-
-        // Add headers to all other responses
         foreach ($headers as $key => $value) {
             $response->header($key, $value);
         }
